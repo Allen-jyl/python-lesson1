dd23c4c376d93abf5a0ed41e5dbf5041c9e284fc
jiayilin <jiayilin0102@163.com>
2025-03-28 23:41:23 +0800

    爬虫

--- practice/crawler.py
+++ practice/crawler.py
@@ -4,6 +4,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 "
                   "Safari/537.36 Edg/134.0.0.0"
 }
+
 for start_num in range(0, 250, 25):
     request = requests.get(f"https://movie.douban.com/top250?start{start_num}", headers=headers)
     html = request.text
